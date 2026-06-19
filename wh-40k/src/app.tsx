c1240d1664f593647e0983af20a77cdde6902a5e
opiliper <opiliper@vivaldi.net>
2026-06-19 16:01:01 +0700

    tailwind css added

--- wh-40k/src/app.tsx
+++ wh-40k/src/app.tsx
@@ -5,18 +5,8 @@ export default function App() {
   const [count, setCount] = createSignal(0);
 
   return (
-    <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)} type="button">
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+    <div class="w-15 h-10 bg-red-500">
+      Text
+    </div>
   );
 }
